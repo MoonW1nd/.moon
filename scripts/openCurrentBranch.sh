ec4981c9073f20ec5a4c9ba945ea64d8122fb689
Alexander Ivankov <moonw1nd@yandex-team.ru>
2020-12-08 21:18:03 +0300

    feat(openCurrentBranch.sh): support ssh protocol

--- scripts/openCurrentBranch.sh
+++ scripts/openCurrentBranch.sh
@@ -1,7 +1,7 @@
 function open-current-branch() {
     get_branch_name_from_ref='sed "s/refs\/heads\///"'
     branch_name=$(git symbolic-ref -q HEAD 2>/dev/null | eval ${get_branch_name_from_ref})
-    repository_url=$(git config --get remote.origin.url | sed s/.git$//)
+    repository_url=$(git config --get remote.origin.url | sed -E -e 's+https://|http://|^git@|.git$++g' | sed 's+:+/+g' | sed 's+^+http://+')
 
     if remote=$(git config --get "branch.${branch_name}.remote"); then
         remote_branch_name="$(git config --get branch.${branch_name}.merge | eval ${get_branch_name_from_ref})"
